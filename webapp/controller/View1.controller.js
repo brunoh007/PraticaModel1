010cb60d5a45e0d0f6f0276bfe940495aeaef5f0
Bruno Franca <bruno.franca@nttdata.com>
2024-11-08 21:10:49 +0000

    atividade incompleta

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,21 +1,28 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
 ],
-function (Controller) {
+function (Controller, Fragment, JSONModel, MessageToast) {
     "use strict";
 
     return Controller.extend("mentoria.fiori.ka.zpraticamodelbf.controller.View1", {
         onInit: function () {
-            var oModelJson = new JSONModel({
-                dataTable: [],
-                dataForm: {
-                    nome, 
-                    dtNasc, 
-                    sexo,
-                    altura
-                }
-            })
-            this.getView().setModel(oModelJson, "model1");
+            // var oModelJson = new JSONModel({
+            //     dataTable: [],
+            //     dataForm: {
+            //         nome, 
+            //         dtNasc, 
+            //         sexo,
+            //         altura
+            //     }
+            // })
+            // this.getView().setModel(oModelJson, "model1");
+
+            var oRegisterJson = new JSONModel();
+            oRegisterJson.loadData("/model/Registers.json");
+            this.getView().setModel(oRegisterJson, "oRegisterJson");
         },
 
         onClosePopup: function () {
@@ -25,11 +32,11 @@ function (Controller) {
         onInsertRegister: function () {
 
             var oView = this.getView(),
-                oDialogKids = this.getView().byId("dialogRegister");
-            if (!oDialogKids) {
+                oDialogRegister = this.getView().byId("dialogRegister");
+            if (!oDialogRegister) {
                 Fragment.load({
                     id: oView.getId(),
-                    name: "mentoria.fiori.ka.zpraticamodelbf.view.dialogRegister",
+                    name: "mentoria.fiori.ka.zpraticamodelbf.view.DialogRegister",
                     type: "XML",
                     controller: this
                 }).then(function (oDialog) {
@@ -37,10 +44,14 @@ function (Controller) {
                     oDialog.open();
                 })
             } else {
-                oDialogKids.open();
+                oDialogRegister.open();
             }
 
         },
+
+        onSaveRegister: function(){
+            this.getView().byId("dialogRegister").close();
+        }
 
         
     });
