826e34915735a07a1867007596936ec3da0ecb59
Bruno Franca <bruno.franca@nttdata.com>
2024-11-08 19:28:41 +0000

    t

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,9 +7,41 @@ function (Controller) {
     return Controller.extend("mentoria.fiori.ka.zpraticamodelbf.controller.View1", {
         onInit: function () {
             var oModelJson = new JSONModel({
-                name: 'Teste',
-                showSecondName: true
+                dataTable: [],
+                dataForm: {
+                    nome, 
+                    dtNasc, 
+                    sexo,
+                    altura
+                }
             })
-        }
+            this.getView().setModel(oModelJson, "model1");
+        },
+
+        onClosePopup: function () {
+            this.getView().byId("dialogRegister").close();
+        },
+
+        onInsertRegister: function () {
+
+            var oView = this.getView(),
+                oDialogKids = this.getView().byId("dialogRegister");
+            if (!oDialogKids) {
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "mentoria.fiori.ka.zpraticamodelbf.view.dialogRegister",
+                    type: "XML",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                })
+            } else {
+                oDialogKids.open();
+            }
+
+        },
+
+        
     });
 });
